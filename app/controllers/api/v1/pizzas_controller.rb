43bce7665a765979077f2c1856e2780a3e098546
ltciro <ltciro@gmail.com>
2017-10-14 21:37:30 -0500

    indentation

--- app/controllers/api/v1/pizzas_controller.rb
+++ app/controllers/api/v1/pizzas_controller.rb
@@ -1,25 +1,25 @@
 module Api 
-	module V1 
-		class PizzasController < ApplicationController
-			def index
-				@pizzas = Pizza.order('created_at DESC')
-				render json: @pizzas
-			end
-			def create
-				@pizza = Pizza.new(pizza_params)
-				if @pizza.save
-					render status: :created
-				else
-					render json: @pizza.errors, status: :unprocessable_entity
-				end
-			end
-			
-			private
-			
-			def pizza_params
-				params.require(:pizza).permit(:name, :ingredients)
-			end
-		end 
-	end
+  module V1 
+    class PizzasController < ApplicationController
+      def index
+        @pizzas = Pizza.order('created_at DESC')
+        render json: @pizzas
+      end
+      def create
+        @pizza = Pizza.new(pizza_params)
+        if @pizza.save
+          render status: :created
+        else
+          render json: @pizza.errors, status: :unprocessable_entity
+        end
+      end
+      
+      private
+      
+      def pizza_params
+        params.require(:pizza).permit(:name, :ingredients)
+      end
+    end 
+  end
 end
 
